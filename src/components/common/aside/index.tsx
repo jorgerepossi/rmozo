ee3ea3ab57e118e57dc965740099c7db382a6c70
Jo Repossi <jorge.repossi@folionet.com>
2024-07-27 19:25:27 -0300

    refactor: aside width

--- src/components/common/aside/index.tsx
+++ src/components/common/aside/index.tsx
@@ -9,7 +9,7 @@ const Aside = () => {
   const { isAsideOpen } = useToggleAside();
   return (
     <aside
-      className={`grid ${isAsideOpen ? "w-[90px]" : "w-[200px]"} w-full flex-shrink-0 grid-rows-[auto_1fr_auto] border-r-[1px] border-neutral-n10 bg-white px-2 py-4 transition-all`}
+      className={`grid ${isAsideOpen ? "w-[90px]" : "w-[200px]"} flex-shrink-0 grid-rows-[auto_1fr_auto] border-r-[1px] border-neutral-n10 bg-white px-2 py-4 transition-all`}
     >
       <Logo />
       <AsideMenu />
